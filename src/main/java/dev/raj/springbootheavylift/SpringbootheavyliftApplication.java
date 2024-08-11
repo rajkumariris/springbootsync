63aaebbb67865fc19104e1f94a25228da92bafa2
rajkumarakula <bhavaniomega1998@gmail.com>
2024-08-11 17:45:25 +0530

    firstheavylift commit

---
+++ src/main/java/dev/raj/springbootheavylift/SpringbootheavyliftApplication.java
@@ -0,0 +1,13 @@
+package dev.raj.springbootheavylift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootheavyliftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootheavyliftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/raj/springbootheavylift/SpringbootheavyliftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raj.springbootheavylift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootheavyliftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
